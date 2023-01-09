e354515d7d0759592ffae1fbccc4ead95b22de9d
samuel-1184 <qizh@datarho.tech>
2023-01-09 17:03:10 +0800

    fix react tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,22 +1,17 @@
-// Add any needed widget imports here (or from controls)
-// import {} from "@jupyter-widgets/base";
-
-import { ExampleModel } from "..";
+import { SqlCellModel } from "..";
 import { createTestModel } from "./utils";
 
-describe("Example", () => {
-    describe("ExampleModel", () => {
-        it("should be createable", () => {
-            const model = createTestModel(ExampleModel);
-            expect(model).toBeInstanceOf(ExampleModel);
-            expect(model.get("value")).toEqual("Hello World");
-        });
+describe("Analytical Sql Cell", () => {
+    it("should be creatable", () => {
+        const model = createTestModel(SqlCellModel);
+        expect(model).toBeInstanceOf(SqlCellModel);
+        expect(model.get("value")).toEqual(undefined);
+    });
 
-        it("should be createable with a value", () => {
-            const state = { value: "Foo Bar!" };
-            const model = createTestModel(ExampleModel, state);
-            expect(model).toBeInstanceOf(ExampleModel);
-            expect(model.get("value")).toEqual("Foo Bar!");
-        });
+    it("should be creatable with a value", () => {
+        const state = { value: "" };
+        const model = createTestModel(SqlCellModel, state);
+        expect(model).toBeInstanceOf(SqlCellModel);
+        expect(model.get("value")).toEqual("");
     });
 });
