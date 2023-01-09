0027d75478675c3798e4eed7639183950132f259
samuel-1184 <qizh@datarho.tech>
2023-01-09 16:55:01 +0800

    fix unit test

--- asqlcell/datasets.py
+++ asqlcell/datasets.py
@@ -5,7 +5,7 @@ def get_random_data(number = 10):
     return pd.DataFrame(
         data = {
             "id": np.arange(number),
-            "price": [np.random.rand() * 100 for i in range(number)],
+            "price": [i for i in range(number)],
             "normal": [True for i in range(number)]
         },
         index = np.arange(number)
