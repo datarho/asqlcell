92c486081fd886c8a364b452edac95ff7d244727
linbendatarho <linben@datarho.tech>
2023-04-25 08:50:43 +0800

    cat to 4

--- asqlcell/utils.py
+++ asqlcell/utils.py
@@ -53,7 +53,7 @@ def get_histogram(df):
                 bins = []
                 sum = 0
                 for i, si in enumerate(sorted_indexes):
-                    if i < 5:
+                    if i < 4:
                         bins.append({"bin" : str(unique_values[si]), "count" : value_counts[si].item()})
                     else:
                         sum += value_counts[si].item()
