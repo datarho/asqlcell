179b8cb9c57356b45f003df5c5f357ae8b8c6881
Zhuhd233 <zhuhd@shyykjhhqy1.wecom.work>
2023-06-09 13:10:51 +0800

    fix for building

--- src/public/function.tsx
+++ src/public/function.tsx
@@ -3,6 +3,7 @@ import { ColItem } from "../visualization";
 // The reason why we pass model as a parameter into this function is 
 // it would cause an error if we get model by useModel here. 
 export const sendVisSql = (model: any, ColName: string, array: ColItem[]) => {
+    const isIndex = ColName === "Index";
     const group = array
         .filter((item: ColItem) => (item.colName !== ""))
         .map((item: ColItem) => (
@@ -10,8 +11,8 @@ export const sendVisSql = (model: any, ColName: string, array: ColItem[]) => {
         ))
     model?.set("vis_sql", [
         // NOTE: THE CONDITION WOULD ALWAYS BE TRUE
-        `select * EXCLUDE (index_rn1qaz2wsx)\nfrom \n(\nSELECT ${group.join(",")}${ColName !== "Index" ? "," + `"${ColName}"` : ""}, ROW_NUMBER() OVER () AS index_rn1qaz2wsx\n FROM $$__NAME__$$ ${true ? "" : "GROUP BY " + `"${ColName}"`}\n)\nusing SAMPLE reservoir (500 rows) REPEATABLE(42)\norder by index_rn1qaz2wsx`,
-        ColName === "Index" ? "index_rn1qaz2wsx" : ColName, // Unexpected constant condition  no-constant-condition
+        `select * EXCLUDE (index_rn1qaz2wsx)\nfrom \n(\nSELECT ${group.join(",")}${isIndex ? "," + `"${ColName}"` : ""}, ROW_NUMBER() OVER () AS index_rn1qaz2wsx\n FROM $$__NAME__$$ \n)\nusing SAMPLE reservoir (500 rows) REPEATABLE(42)\norder by index_rn1qaz2wsx`,
+        isIndex ? "index_rn1qaz2wsx" : ColName, // Unexpected constant condition  no-constant-condition
         new Date().toISOString()
     ]);
     model?.save_changes();
