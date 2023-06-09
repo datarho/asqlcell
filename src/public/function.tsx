4d3a2b10f7ea8bfac0eb98aca8fcdf0ad7689608
Zhuhd233 <zhuhd@shyykjhhqy1.wecom.work>
2023-06-09 12:38:42 +0800

    fix for review - 3

--- src/public/function.tsx
+++ src/public/function.tsx
@@ -10,7 +10,7 @@ export const sendVisSql = (model: any, ColName: string, array: ColItem[]) => {
         ))
     model?.set("vis_sql", [
         // NOTE: THE CONDITION WOULD ALWAYS BE TRUE
-        `select * EXCLUDE (index_rn1qaz2wsx)\nfrom \n(\nSELECT ${group.join(",")}${!(ColName === "Index") ? "," + `"${ColName}"` : ""}, ROW_NUMBER() OVER () AS index_rn1qaz2wsx\n FROM $$__NAME__$$ ${true ? "" : "GROUP BY " + `"${ColName}"`}\n)\nusing SAMPLE reservoir (500 rows) REPEATABLE(42)\norder by index_rn1qaz2wsx`,
+        `select * EXCLUDE (index_rn1qaz2wsx)\nfrom \n(\nSELECT ${group.join(",")}${ColName !== "Index" ? "," + `"${ColName}"` : ""}, ROW_NUMBER() OVER () AS index_rn1qaz2wsx\n FROM $$__NAME__$$ ${true ? "" : "GROUP BY " + `"${ColName}"`}\n)\nusing SAMPLE reservoir (500 rows) REPEATABLE(42)\norder by index_rn1qaz2wsx`,
         ColName === "Index" ? "index_rn1qaz2wsx" : ColName, // Unexpected constant condition  no-constant-condition
         new Date().toISOString()
     ]);
