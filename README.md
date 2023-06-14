1d42078dec49e6d582aa61fada182dca768354bf
samuel0x0 <qizh@datarho.tech>
2023-06-14 22:06:46 +0800

    fix develop install

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ Mac OS and Windows Subsystem for Linux.
 Please run the following commands to create a conda environment:
 
 ```bash
-conda create -n asqlcell -c conda-forge nodejs=18.15 python=3.10 jupyterlab=3
+conda create -n asqlcell -c conda-forge nodejs=18.15 python=3.10 jupyterlab=3.6 jupyter_packaging=0.12
 conda activate asqlcell
 ```
 
--- asqlcell/__init__.py
+++ asqlcell/__init__.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Copyright (c) qizh.
-# Distributed under the terms of the Modified BSD License.
-from .asqlcell import SqlcellWidget
-from ._version import __version__, version_info
-
 def _jupyter_labextension_paths():
     """Called by Jupyter Lab Server to detect if it is a valid labextension and
     to install the widget
@@ -18,10 +10,12 @@ def _jupyter_labextension_paths():
         from `src` directory into <jupyter path>/labextensions/<dest> directory
         during widget installation
     """
-    return [{
-        'src': 'labextension',
-        'dest': 'asqlcell',
-    }]
+    return [
+        {
+            "src": "labextension",
+            "dest": "asqlcell",
+        }
+    ]
 
 
 def _jupyter_nbextension_paths():
@@ -40,9 +34,4 @@ def _jupyter_nbextension_paths():
     require: Path to importable AMD Javascript module inside the
         <jupyter path>/nbextensions/<dest> directory
     """
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension',
-        'dest': 'asqlcell',
-        'require': 'asqlcell/extension'
-    }]
+    return [{"section": "notebook", "src": "nbextension", "dest": "asqlcell", "require": "asqlcell/extension"}]
