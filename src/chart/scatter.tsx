827d94c0ccb7eaf2de77fc6fcd0c4bfc6c502bd4
samuel-1184 <qizh@datarho.tech>
2023-08-11 17:29:51 +0800

    fix area and scatter

--- src/chart/scatter.tsx
+++ src/chart/scatter.tsx
@@ -1,10 +1,10 @@
+import { Stack, Text } from "@mantine/core";
 import React, { FunctionComponent } from "react";
-import { Text } from "@mantine/core";
 import { FieldSwitch } from "./properties";
 
 export const ScatterChart: FunctionComponent = () => {
     return (
-        <>
+        <Stack>
             <Text fw={600}>X-Axis</Text>
 
             <FieldSwitch major="x" minor="y" sort={true} />
@@ -12,6 +12,10 @@ export const ScatterChart: FunctionComponent = () => {
             <Text fw={600}>Y-Axis</Text>
 
             <FieldSwitch major="y" minor="x" sort={true} />
-        </>
+
+            <Text fw={600}>Color</Text>
+
+            <FieldSwitch major="color" clearable />
+        </Stack>
     );
 }
