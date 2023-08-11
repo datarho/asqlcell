e3b1a7f2b9aec9bdea2471c8f86c6d91e0d5484b
Zhuhd233 <zhuhd@shyykjhhqy1.wecom.work>
2023-08-11 17:29:51 +0800

    Update area.tsx

--- src/chart/area.tsx
+++ src/chart/area.tsx
@@ -5,13 +5,13 @@ import { AggregationSwitch, FieldSwitch } from "./properties";
 export const AreaChart: FunctionComponent = () => {
     return (
         <>
-            <Text fw={600}>Y-Axis</Text>
-
-            <FieldSwitch major="y" minor="x" sort={true} />
-
             <Text fw={600}>X-Axis</Text>
 
             <FieldSwitch major="x" minor="y" sort={true} />
+
+            <Text fw={600}>Y-Axis</Text>
+
+            <FieldSwitch major="y" minor="x" sort={true} />
 
             <AggregationSwitch major="x" minor="y" />
 
