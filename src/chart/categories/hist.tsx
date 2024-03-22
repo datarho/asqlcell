61b8229534df57cd15c3b07310d623d256d2b3e0
Zhuhd233 <zhuhd@shyykjhhqy1.wecom.work>
2024-03-22 13:23:24 +0800

    Update hist.tsx
    Hide  interval

--- src/chart/categories/hist.tsx
+++ src/chart/categories/hist.tsx
@@ -51,109 +51,111 @@ export const HistChart: FunctionComponent<HistChartProps> = ({ item, headerConte
         <Stack>
             {
                 data.length > 0 ?
-                    <VegaLite
-                        data={barData}
-                        actions={false}
-                        renderer={'svg'}
-                        spec={{
-                            background: "transparent",
-                            data: {
-                                name: "table"
-                            },
-                            width: 60,
-                            height: 40,
-                            config: {
-                                view: {
-                                    stroke: null
-                                }
-                            },
-                            layer: [
-                                {
-                                    params: [
-                                        {
-                                            name: "hover",
-                                            select: {
-                                                type: "point",
-                                                on: "mouseover",
-                                                clear: "mouseout"
+                    <>
+                        <VegaLite
+                            data={barData}
+                            actions={false}
+                            renderer={'svg'}
+                            spec={{
+                                background: "transparent",
+                                data: {
+                                    name: "table"
+                                },
+                                width: 60,
+                                height: 40,
+                                config: {
+                                    view: {
+                                        stroke: null
+                                    }
+                                },
+                                layer: [
+                                    {
+                                        params: [
+                                            {
+                                                name: "hover",
+                                                select: {
+                                                    type: "point",
+                                                    on: "mouseover",
+                                                    clear: "mouseout"
+                                                }
                                             }
-                                        }
-                                    ],
-                                    mark: {
-                                        type: "bar",
-                                        color: "#eee",
-                                        tooltip: true
-                                    },
-                                    transform: [
-                                        {
-                                            calculate: "datum.a + ': ' +datum.b", as: "tooltip",
-                                        }
-                                    ],
-                                    encoding: {
-                                        x: {
-                                            field: "index",
-                                            type: "nominal",
-                                            axis: {
-                                                labels: false,
-                                                title: null
-                                            },
+                                        ],
+                                        mark: {
+                                            type: "bar",
+                                            color: "#eee",
+                                            tooltip: true
                                         },
-                                        tooltip: {
-                                            field: "tooltip",
-                                            type: "nominal"
-                                        },
-                                        opacity: {
-                                            condition: {
-                                                test: {
-                                                    param: "hover",
-                                                    empty: false
+                                        transform: [
+                                            {
+                                                calculate: "datum.a + ': ' +datum.b", as: "tooltip",
+                                            }
+                                        ],
+                                        encoding: {
+                                            x: {
+                                                field: "index",
+                                                type: "nominal",
+                                                axis: {
+                                                    labels: false,
+                                                    title: null
                                                 },
-                                                value: 0.5
                                             },
-                                            value: 0
-                                        },
-                                        detail: [{ field: "count" }]
-                                    }
-                                },
-                                {
-                                    mark: "bar",
-                                    transform: [{
-                                        calculate: "datum.b===0 ? 0 : datum.b === 1? 0.5: log(datum.b)/log(2)", as: "log_x"
-                                    }],
-                                    encoding: {
-                                        x: {
-                                            field: "index",
-                                            type: "nominal",
-                                            axis: {
-                                                labels: false,
-                                                title: null,
-                                                ticks: false
+                                            tooltip: {
+                                                field: "tooltip",
+                                                type: "nominal"
                                             },
-                                        },
-                                        y: {
-                                            field: "log_x",
-                                            type: "quantitative",
-                                            axis: {
-                                                labels: false,
-                                                domain: false,
-                                                grid: false,
-                                                ticks: false,
-                                                title: null
+                                            opacity: {
+                                                condition: {
+                                                    test: {
+                                                        param: "hover",
+                                                        empty: false
+                                                    },
+                                                    value: 0.5
+                                                },
+                                                value: 0
                                             },
-                                        },
-                                    }
-                                },
-                            ]
-                        }}
-                    />
+                                            detail: [{ field: "count" }]
+                                        }
+                                    },
+                                    {
+                                        mark: "bar",
+                                        transform: [{
+                                            calculate: "datum.b===0 ? 0 : datum.b === 1? 0.5: log(datum.b)/log(2)", as: "log_x"
+                                        }],
+                                        encoding: {
+                                            x: {
+                                                field: "index",
+                                                type: "nominal",
+                                                axis: {
+                                                    labels: false,
+                                                    title: null,
+                                                    ticks: false
+                                                },
+                                            },
+                                            y: {
+                                                field: "log_x",
+                                                type: "quantitative",
+                                                axis: {
+                                                    labels: false,
+                                                    domain: false,
+                                                    grid: false,
+                                                    ticks: false,
+                                                    title: null
+                                                },
+                                            },
+                                        }
+                                    },
+                                ]
+                            }}
+                        />
+                        <Group sx={{ width: "max-content" }}>
+                            <Text size="xs" c={"#696969"} sx={{ marginTop: "-20px" }}>
+                                {globalInterval(item)}
+                            </Text>
+                        </Group>
+                    </>
                     :
                     <></>
             }
-            <Group sx={{ width: "max-content" }}>
-                <Text size="xs" c={"#696969"} sx={{ marginTop: "-20px" }}>
-                    {globalInterval(item)}
-                </Text>
-            </Group>
         </Stack>
     )
 };
